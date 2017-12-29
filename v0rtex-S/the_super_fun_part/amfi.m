4d424893bd8841326e926f3eccf2f808051d30eb
thisiswisy <31823439+thisiswisy@users.noreply.github.com>
2017-12-29 22:05:57 +0100

    Add files via upload

--- v0rtex-S/the_super_fun_part/amfi.m
+++ v0rtex-S/the_super_fun_part/amfi.m
@@ -16,45 +16,6 @@ kern_return_t mach_vm_write(vm_map_t target_task, mach_vm_address_t address, vm_
 kern_return_t mach_vm_allocate(vm_map_t target, mach_vm_address_t *address, mach_vm_size_t size, int flags);
 
 
-size_t
-kread(uint64_t where, void *p, size_t size)
-{
-    
-    int rv;
-    size_t offset = 0;
-    while (offset < size) {
-        mach_vm_size_t sz, chunk = 2048;
-        if (chunk > size - offset) {
-            chunk = size - offset;
-        }
-        rv = mach_vm_read_overwrite(taskfp0, where + offset, chunk, (mach_vm_address_t)p + offset, &sz);
-        if (rv || sz == 0) {
-            fprintf(stderr, "[e] error reading kernel @%p\n", (void *)(offset + where));
-            break;
-        }
-        offset += sz;
-    }
-    return offset;
-}
-
-/*size_t kwrite(uint64_t where, const void *p, size_t size) {
-   // printf("taskfp0: %d", taskfp0);
-        int rv;
-        size_t offset = 0;
-        while (offset < size) {
-                size_t chunk = 2048;
-                if (chunk > size - offset) {
-                        chunk = size - offset;
-                    }
-                rv = mach_vm_write(taskfp0, where + offset, (mach_vm_offset_t)p + offset, chunk);
-                if (rv) {
-                        fprintf(stderr, "[e] error writing kernel @%p\n", (void *)(offset + where));
-                        break;
-                    }
-                offset += chunk;
-            }
-        return offset;
-    }*/
 
 void kwrite32(uint64_t where, uint32_t what) {
     uint32_t _what = what;
